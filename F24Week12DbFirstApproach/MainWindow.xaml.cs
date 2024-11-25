fd59e35aeb0c99def38b0db2964ce4cd6471529e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-11-25 09:53:00 -0500

    find

--- F24Week12DbFirstApproach/MainWindow.xaml.cs
+++ F24Week12DbFirstApproach/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace F24Week12DbFirstApproach
     /// </summary>
     public partial class MainWindow : Window
     {
+        // create an instance of the context class
+        SchoolDBEntities db = new SchoolDBEntities();
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadStandardsInCombobox();
+        }
+
+        private void LoadStudents()
+        {
+            var students = db.Students.ToList();
+            grdStudents.ItemsSource = students;
+        }
+
+        private void LoadStandardsInCombobox()
+        {
+            var standards = db.Standards.ToList();
+            cmbStandard.ItemsSource = standards;
+            cmbStandard.DisplayMemberPath = "StandardName";
+            cmbStandard.SelectedValuePath = "StandardId";
+        }
+
+        private void btnLoadStudents_Click(object sender, RoutedEventArgs e)
+        {
+            LoadStudents();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var std = db.Students.Find(id);
+
+            if (std != null)
+            {
+                txtName.Text = std.StudentName;
+                cmbStandard.SelectedValue = std.StandardId;
+            }
+            else
+            {
+                txtName.Text = "";
+                cmbStandard.SelectedIndex = -1;
+                MessageBox.Show("Invalid ID. Please try again.");
+            }
         }
     }
 }
